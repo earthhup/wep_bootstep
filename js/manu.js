9fcd334fbbf33b40c57db26fbe3bd9f476dba7b5
earthhup <sriteerachai@gmail.com>
2026-09-13 00:12:30 -0700

    Add files via upload

---
+++ js/manu.js
@@ -0,0 +1,19 @@
+
+   // โหลด menu1.js
+   fetch("manu.html")
+   .then(res => res.text())
+   .then(data => {
+       document.getElementById("manu1").innerHTML = data;
+
+       // ✅ re-bind toggle หลังจากเมนูถูก inject
+       const sidebarToggle = document.body.querySelector('#sidebarToggle');
+       if (sidebarToggle) {
+           sidebarToggle.addEventListener('click', event => {
+               event.preventDefault();
+               document.body.classList.toggle('sb-sidenav-toggled');
+           });
+       }
+   });
+
+
+
